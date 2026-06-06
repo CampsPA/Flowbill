dcb8be54250742cf8dd87c3c48f702447763d470
Paul Campos <campos.paul@outlook.com>
2026-06-06 16:26:49 -0400

    fix conftest to use settings.database_name

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ from app.auth.schemas import UserCreate
 
 
 # Database connection
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.test_database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 test_engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
